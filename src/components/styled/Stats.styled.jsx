a9f4effe175d437c8c9174ddba7e5c8b66149b92
bengin34 <becaglar3434@gmail.com>
2023-03-03 17:21:14 +0300

    stats

---
+++ src/components/styled/Stats.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content:space-between;
+border-bottom: 2px solid #808080;
+margin: .6rem 0;
+padding-bottom: .5rem;
+`;
+
+export default Div;
